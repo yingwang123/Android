3bc0462358739c89c809da3ce3ab20866ce3f6a0
yingwang <1070957833@qq.com>
2017-04-11 18:33:19 +0800

    intent使用问题，加载器loader的使用

---
+++ workspace1/MyApplication/app/src/main/java/com/example/user/myapplication/activies/LoaderActivity.java
@@ -0,0 +1,79 @@
+package com.example.user.myapplication.activies;
+
+import android.app.Activity;
+import android.app.LoaderManager;
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.text.TextUtils;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+
+import com.example.user.myapplication.R;
+
+/**
+ * Created by user on 2017/4/11.
+ */
+public class LoaderActivity extends Activity implements LoaderManager.LoaderCallbacks<Cursor> {
+    SimpleCursorAdapter adapter;
+    private ListView lv_contants;
+    private String mCurFilter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loader);
+        lv_contants = (ListView) findViewById(R.id.lv_contants);
+        adapter = new SimpleCursorAdapter(this,
+                android.R.layout.simple_list_item_2, null,
+                new String[] { ContactsContract.Contacts.DISPLAY_NAME, ContactsContract.Contacts.CONTACT_STATUS },
+                new int[] { android.R.id.text1, android.R.id.text2 }, 0);
+        lv_contants.setAdapter(adapter);
+        getLoaderManager().initLoader(0, null, this);
+
+    }
+
+    public boolean onQueryTextChange(String newText) {
+        mCurFilter = !TextUtils.isEmpty(newText) ? newText : null;
+        getLoaderManager().restartLoader(0, null, this);
+        return true;
+    }
+
+    static final String[] CONTACTS_SUMMARY_PROJECTION = new String[] {
+            ContactsContract.Contacts._ID,
+            ContactsContract.Contacts.DISPLAY_NAME,
+            ContactsContract.Contacts.CONTACT_STATUS,
+            ContactsContract.Contacts.CONTACT_PRESENCE,
+            ContactsContract.Contacts.PHOTO_ID,
+            ContactsContract.Contacts.LOOKUP_KEY,
+    };
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+
+        Uri baseUri;
+        if (mCurFilter != null) {
+            baseUri = Uri.withAppendedPath(ContactsContract.Contacts.CONTENT_FILTER_URI,
+                    Uri.encode(mCurFilter));
+        } else {
+            baseUri = ContactsContract.Contacts.CONTENT_URI;
+        }
+
+        String select = "((" + ContactsContract.Contacts.DISPLAY_NAME + " NOTNULL) AND ("
+                + ContactsContract.Contacts.HAS_PHONE_NUMBER + "=1) AND ("
+                + ContactsContract.Contacts.DISPLAY_NAME + " != '' ))";
+        return new CursorLoader(getApplicationContext(), baseUri,
+                CONTACTS_SUMMARY_PROJECTION, select, null,
+                ContactsContract.Contacts.DISPLAY_NAME + " COLLATE LOCALIZED ASC");
+    }
+
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+
+        adapter.swapCursor(data);
+    }
+
+    public void onLoaderReset(Loader<Cursor> loader) {
+
+        adapter.swapCursor(null);
+    }
+}
